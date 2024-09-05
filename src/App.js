8cf8419a8c3f1ea18a2db8c399ba16118644e9cd
Suzuki tomoya <suzy_tomy@docomo.ne.jp>
2024-09-05 16:50:30 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Hedder from "./compornent/common/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hedder />
+      ポートフォリオです。
+      developbranchマージしました。
+      git fech,margeテストします。
     </div>
   );
 }
